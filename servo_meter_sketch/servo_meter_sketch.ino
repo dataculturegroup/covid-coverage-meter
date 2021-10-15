28777f4caf074f8bd01479644e283f4490f423c9
Rahul B <rahulbot@gmail.com>
2021-10-15 15:48:43 -0400

    works with static data

--- servo_meter_sketch/servo_meter_sketch.ino
+++ servo_meter_sketch/servo_meter_sketch.ino
@@ -5,6 +5,10 @@
 
 boolean TRY_WIFI = false;
 
+#define DATA_SIZE 90
+
+int coverageData[DATA_SIZE] = {11,13,13,12,25,34,60,74,76,70,68,68,65,66,63,65,62,53,44,45,45,48,47,48,47,49,50,50,43,42,40,39,37,35,34,39,41,36,35,35,32,35,38,37,38,39,39,40,39,30,31,34,35,33,30,31,31,33,34,31,30,31,29,27,24,27,26,25,24,24,24,22,22,21,21,21,24,26,27,29,28,25,26,24,24,23,23,22,21,21};
+
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
@@ -42,45 +46,70 @@ void setup() {
   }
 }
 
-int scale(int input) {
-  // input 1023-0, output 180-0
-  float rangeWindow = 180 / 7;
+int potPositionToDataIndex(int input) {
+  // input 1023-0, output 100-0
+  float rangeWindow = (DATA_SIZE-1) / 7;
   float ratio;
-  int range; // the output
-  if (input > 981) {
-    range = map(input, 981, 1023, 154, 180);
+  int index; // the output
+  if (input > 1010) {
+    index = DATA_SIZE-1;
+  } else if (input > 981) {
+    index = map(input, 981, 1023, rangeWindow*6, max(DATA_SIZE-1, rangeWindow*7));
   } else if (input > 776) {
-    range = map(input, 776, 981, 128, 154);
+    index = map(input, 776, 981, rangeWindow*5, rangeWindow*6);
   } else if (input > 612) {
-    range = map(input, 612, 776, 102, 128);
+    index = map(input, 612, 776, rangeWindow*4, rangeWindow*5);
   } else if (input > 443) {
-    range = map(input, 443, 612, 77, 102);
+    index = map(input, 443, 612, rangeWindow*3, rangeWindow*4);
   } else if (input > 275) {
-    range = map(input, 275, 443, 51, 77);    
+    index = map(input, 275, 443, rangeWindow*2, rangeWindow*3);
   } else if (input > 75) {
-    range = map(input, 75, 275, 25, 50);
+    index = map(input, 75, 275, rangeWindow*1, rangeWindow*2);
   } else {
-    range = map(input, 0, 75, 0, 25);
+    index = map(input, 0, 75, 0, rangeWindow);
   }
-  return range;
+  return index;
 }
 
-int val;
+int lastSensorValue = 0;
+int sensorValue;
+int dataIndex;
+int dataValue;
+int servoPosition;
 
 void loop() {
   // check the network connection once every 10 seconds:
   // delay(10000);
   // printData();
   // Serial.println("----------------------------------------");
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  Serial.print("Sensor: ");
-  Serial.print(val);
-  //val = map(val, 0, 1023, 0, 180);     // scale it for use with the servo (value between 0 and 180)
-  val = scale(val);
-  myservo.write(val);                  // sets the servo position according to the scaled value
-  Serial.print(" -> ");
-  Serial.println(val);
-  delay(1000);                           // waits for the servo to get there
+
+  // check the pot to see what date they want
+  sensorValue = analogRead(potpin);
+  if (abs(sensorValue - lastSensorValue) > 10) { // sensor is noisy, so use a dinwo
+    Serial.println("-------------------------------------------------------");
+
+    Serial.print("Sensor: ");
+    Serial.println(sensorValue);
+  
+    // translate the pot position into an index in to the data array
+    dataIndex = potPositionToDataIndex(sensorValue);
+    Serial.print("Data Index: ");
+    Serial.println(dataIndex);
+  
+    // figure out the current selected data based on the index
+    dataValue = coverageData[dataIndex];
+    Serial.print("Data Value: ");
+    Serial.println(dataValue);
+  
+    // map from selected data value to servo position
+    servoPosition = map(dataValue, 0, 100, 0, 180);
+    myservo.write(servoPosition);
+    Serial.print("Servo Position: ");
+    Serial.println(servoPosition);
+    
+    lastSensorValue = sensorValue;
+    delay(100); // waits for the servo to get there
+  }
 }
 
 void printWifiData() {
